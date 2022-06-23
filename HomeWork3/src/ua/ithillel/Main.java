158514341ac15cf8335ef0b49e9ea84c42e96a71
Dement1cs <93581822+Dement1cs@users.noreply.github.com>
2022-06-23 21:10:15 +0100

    Add files via upload

---
+++ HomeWork3/src/ua/ithillel/Main.java
@@ -0,0 +1,27 @@
+package ua.ithillel;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b = 5;
+        short s = 25;
+        int i = 10_000_000;
+        long l = 14_235_324_146L;
+        float f = 100.066_123_455f;
+        double d = 15_500_088.443_311_220_1;
+        char ch = ('\u00a9');
+        char ch1 = ('\u00ae');
+        char ch2 = ('\u00b5');
+
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(ch1);
+        System.out.println(ch2);
+    }
+}
